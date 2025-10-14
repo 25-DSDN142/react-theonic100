d2029c9cf4e92427362edf9992752ceaa7342346
theonic100 <theo.nicholson.nz.100@gmail.com>
2025-10-14 21:18:09 +1300

    Update 0_displaySettings.js

--- 0_displaySettings.js
+++ 0_displaySettings.js
@@ -22,8 +22,8 @@
 
 
 // If using video file use these dimentions: 
-// const  CaptureWidth = 1280;
-// const  CaptureHeight = 720;
+const  CaptureWidth = 1280;
+const  CaptureHeight = 720;
 
 //const  CaptureWidth = 640;
 // const  CaptureHeight = 360;
@@ -32,15 +32,15 @@
 // If using Webcam use these dimentions
 
 
-const  CaptureWidth = 1280;
- const  CaptureHeight = 960;
+// const  CaptureWidth = 1280;
+//  const  CaptureHeight = 960;
 
 
 
 // program Mode
-const webCam = true; // set to false to use video
+const webCam = false; // set to false to use video
 const videoFile = "hands1.mov" // update this to match the video file you want to load
-const flipVideo = false; // changes from mirror mode to standard video mode
+const flipVideo = true; // changes from mirror mode to standard video mode
 
 
 // global variables 
